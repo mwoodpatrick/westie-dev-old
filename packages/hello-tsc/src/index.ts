d323164f950109e32f60de958dbb1419b1b3d48a
mwoodpatrick <mwoodpatrick@gmail.com>
2022-08-02 18:51:59 -0700

    Add  node

--- packages/hello-tsc/src/index.ts
+++ packages/hello-tsc/src/index.ts
@@ -1,1 +1,10 @@
 export * from './lib/hello-tsc';
+// file: packages/tsapp/src/index.ts
+
+// importing from hello-tsc
+import { helloTsc } from '@happynrwl/hello-tsc';
+
+// use the function
+helloTsc();
+
+console.log(`Running ${tsapp()}`);
