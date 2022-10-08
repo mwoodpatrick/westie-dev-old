529411cdc991778889d6cbf4b948671a62ac2817
mwoodpatrick <mwoodpatrick@gmail.com>
2022-10-08 15:32:25 -0700

    Add function to log requesting URL

--- apps/simple-express-app/src/main.ts
+++ apps/simple-express-app/src/main.ts
@@ -11,6 +11,12 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to simple_express_app!' });
 });
 
+app.use((req, res, next) => {
+  // console.log('Time:', Date.now())
+  console.log('Request URL: ', req.originalUrl)
+  next()
+})
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
