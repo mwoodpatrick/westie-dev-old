dd16c55dfdcdd5371772bf57c82dbcc878e78839
mwoodpatrick <mwoodpatrick@gmail.com>
2022-11-26 04:35:53 -0800

    update nrwl remix plugin

--- packages/nx-labs/nrwl/packages/remix/src/generators/application/application.impl.ts
+++ packages/nx-labs/nrwl/packages/remix/src/generators/application/application.impl.ts
@@ -28,19 +28,20 @@ export default async function (tree: Tree, _options: NxRemixGeneratorSchema) {
   const installTask = addDependenciesToPackageJson(
     tree,
     {
-      '@remix-run/react': '^1.6.8',
-      '@remix-run/node': '^1.6.8',
+      '@remix-run/react': '^1.7.6',
+      '@remix-run/node': '^1.7.6',
       react: '^18.2.0',
       'react-dom': '^18.2.0',
-      '@remix-run/serve': '^1.6.8',
+      remix: '^1.7.6',
+      '@remix-run/serve': '^1.7.6',
     },
     {
-      '@remix-run/dev': '^1.6.8',
-      '@remix-run/eslint-config': '^1.6.8',
-      '@types/react': '^18.0.15',
-      '@types/react-dom': '^18.0.6',
-      eslint: '^8.20.0',
-      typescript: '^4.7.4',
+      '@remix-run/dev': '^1.7.6',
+      '@remix-run/eslint-config': '^1.7.6',
+      '@types/react': '^18.0.20',
+      '@types/react-dom': '^18.0.9',
+      eslint: '^8.28.0',
+      typescript: '^4.8.4',
     }
   );
   tasks.push(installTask);
